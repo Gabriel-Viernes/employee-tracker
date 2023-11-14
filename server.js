17c040525bd41e87140db4c3748486cd191f0414
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-14 11:53:47 -0600

    imported mysql2 and began work on doing mysql queries for js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const db = require('./js/query.js')
+
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'spottedleaf',
+        database:'employees_db'
+    },
+    console.log(`Connected to database`)
+)
+
+
+function uiStart () {
+    inquirer.prompt([
+        {
+            type:'list',
+            message:'What would you like to do?',
+            name:'menuSelect',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])
+}
+uiStart()
